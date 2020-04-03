7e114b29c85e682bfe1b50f76098c5f3a21d4b11
Roger Bambinetti <rogerbambinetti@hotmail.com>
2020-04-03 14:52:20 -0300

    login started

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState, useMemo } from 'react';
+
+import api from '../services/api';
+
+import './Login.css';
+
+export default function Chat() {
+
+    const [input, setInput] = useState('');
+
+    return (
+        <>
+          
+        </>
+    );
+
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+
+import Login from './pages/Login';
 import Chat from './pages/Chat';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Chat} />
+                <Route path="/" exact component={Login} />
+                <Route path="/chat" exact component={Chat} />
             </Switch>
         </BrowserRouter>
     );
